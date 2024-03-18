3b0c957448823314919bd75a6e68047cbe384bfb
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2024-03-18 14:02:57 +0300

    feat: add create-user.dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString, IsUrl, Length } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({ example: 'exampleuser' })
+  @IsString()
+  @Length(1, 64)
+  username: string;
+
+  @ApiProperty({ example: 'exampleuser' })
+  @IsString()
+  @Length(0, 200)
+  about: string;
+
+  @ApiProperty({ example: 'https://i.pravatar.cc/150?img=3' })
+  @IsUrl()
+  @Length(0, 200)
+  avatar: string;
+
+  @ApiProperty({ example: 'user@yandex.ru' })
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: 'somestrongpassword' })
+  @Length(2)
+  password: string;
+}
