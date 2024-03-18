9571e24262949bde37e7d342130425947067c7dd
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2024-03-18 19:25:49 +0300

    feat: add wishes DTOs

--- src/wishes/dto/create-wish.dto.ts
+++ src/wishes/dto/create-wish.dto.ts
@@ -1,1 +1,12 @@
-export class CreateWishDto {}
+import { OmitType } from '@nestjs/swagger';
+import { WishDto } from './wish.dto';
+
+export class CreateWishDto extends OmitType(WishDto, [
+  'id',
+  'createdAt',
+  'updatedAt',
+  'raised',
+  'owner',
+  'offers',
+  'copied',
+]) {}
