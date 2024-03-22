621b84984786d205f2064b1e88f1d58aed9fdb5c
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2024-03-22 17:54:03 +0300

    feat: add basic CRUD operations for wishlists

--- src/wishlists/wishlists.controller.ts
+++ src/wishlists/wishlists.controller.ts
@@ -1,34 +1,52 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { WishlistsService } from './wishlists.service';
 import { CreateWishlistDto } from './dto/create-wishlist.dto';
 import { UpdateWishlistDto } from './dto/update-wishlist.dto';
+import { AuthUser } from 'src/common/decorators/user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { Wishlist } from './entities/wishlist.entity';
 
 @Controller('wishlists')
 export class WishlistsController {
   constructor(private readonly wishlistsService: WishlistsService) {}
 
   @Post()
-  create(@Body() createWishlistDto: CreateWishlistDto) {
-    return this.wishlistsService.create(createWishlistDto);
+  create(
+    @AuthUser() user: User,
+    @Body() dto: CreateWishlistDto,
+  ): Promise<Wishlist> {
+    return this.wishlistsService.create(user, dto);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<Wishlist[]> {
     return this.wishlistsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<Wishlist> {
     return this.wishlistsService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWishlistDto: UpdateWishlistDto) {
-    return this.wishlistsService.update(+id, updateWishlistDto);
+  update(
+    @AuthUser() user: User,
+    @Param('id') id: string,
+    @Body() dto: UpdateWishlistDto,
+  ) {
+    return this.wishlistsService.update(+id, dto, +user.id);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.wishlistsService.remove(+id);
+  remove(@AuthUser() user: User, @Param('id') id: string) {
+    return this.wishlistsService.remove(+id, +user.id);
   }
 }
--- src/wishlists/wishlists.service.ts
+++ src/wishlists/wishlists.service.ts
@@ -1,26 +1,98 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateWishlistDto } from './dto/create-wishlist.dto';
 import { UpdateWishlistDto } from './dto/update-wishlist.dto';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { Wishlist } from './entities/wishlist.entity';
+import { Wish } from 'src/wishes/entities/wish.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class WishlistsService {
-  create(createWishlistDto: CreateWishlistDto) {
-    return 'This action adds a new wishlist';
+  constructor(
+    @InjectRepository(Wishlist)
+    private wishlistsRepository: Repository<Wishlist>,
+  ) {}
+
+  async create(user: User, dto: CreateWishlistDto): Promise<Wishlist> {
+    const { name, image, itemsId } = dto;
+    const wishes = itemsId.map((id) => ({ id } as Wish));
+
+    const wishlist = this.wishlistsRepository.create({
+      name,
+      image,
+      owner: user,
+      items: wishes,
+    });
+
+    if (!wishlist) {
+      throw new BadRequestException('Wishlist not created');
+    }
+
+    return this.wishlistsRepository.save(wishlist);
   }
 
-  findAll() {
-    return `This action returns all wishlists`;
+  async findAll() {
+    const wishlist = await this.wishlistsRepository.find({
+      relations: ['owner', 'items'],
+    });
+
+    if (wishlist.length === 0) {
+      throw new NotFoundException('Wishlists not found');
+    }
+
+    return wishlist;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} wishlist`;
+  async findOne(id: number) {
+    const wishlist = await this.wishlistsRepository.findOne({
+      where: { id },
+      relations: ['owner', 'items'],
+    });
+
+    if (!wishlist) {
+      throw new NotFoundException('Wishlists not found');
+    }
+
+    return wishlist;
   }
 
-  update(id: number, updateWishlistDto: UpdateWishlistDto) {
-    return `This action updates a #${id} wishlist`;
+  async update(id: number, dto: UpdateWishlistDto, userId: number) {
+    const wishlist = await this.wishlistsRepository.findOne({
+      where: { id },
+      relations: ['owner', 'items'],
+    });
+
+    if (!wishlist) {
+      throw new NotFoundException('Wishlist not found');
+    }
+
+    if (wishlist.owner.id !== userId) {
+      throw new ForbiddenException('You can update only your wishlist');
+    }
+
+    return this.wishlistsRepository.save({ ...dto, id });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} wishlist`;
+  async remove(id: number, userId: number) {
+    const wishlist = await this.wishlistsRepository.findOne({
+      where: { id },
+      relations: ['owner', 'items'],
+    });
+
+    if (!wishlist) {
+      throw new NotFoundException('Wishlist not found');
+    }
+
+    if (wishlist.owner.id !== userId) {
+      throw new ForbiddenException('You can delete only your wishlist');
+    }
+
+    return this.wishlistsRepository.delete(id);
   }
 }
